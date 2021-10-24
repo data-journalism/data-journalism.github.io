b97004f90c88e9ab4741f1aaeaa63b48fcc616b9
chengjun <wangchj04@gmail.com>
2021-10-24 10:36:56 +0800

    notebook

---
+++ git.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# open github master branch
+# git pull first
+# git add .
+# git commit -m 'this is a message'
+# git push origin main
+# ssh-add ~/.ssh/id_rsa
+# vim save and quit :wq
+# git remote set-url origin git@github.com:chengjun/mybook.git
+# open atom master branch
